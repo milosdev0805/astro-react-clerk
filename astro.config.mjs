402b5568f7721704fbd6d0cb72d401f19a3ab6f0
milosdev0805 <mstefanovicand@gmail.com>
2024-04-13 10:06:00 -0700

    Try using netlify backend

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 import astroClerk from "astro-clerk-auth";
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    astroClerk({
-      afterSignInUrl: "/",
-      afterSignUpUrl: "/",
-    }),
-  ],
+  integrations: [react(), astroClerk({
+    afterSignInUrl: "/",
+    afterSignUpUrl: "/"
+  })],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify()
 });
